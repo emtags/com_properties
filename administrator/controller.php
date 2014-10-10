09a5158c4ffb16e12f015e6946f924837c2bd400
Thiago Miranda <theprogramer@theprogramer.com.br>
2014-10-10 11:05:01 -0300

    Update controllers names

--- administrator/controller.php
+++ administrator/controller.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * @package     Joomla.Administrator
- * @subpackage  com_weblinks
+ * @package     Realeza
+ * @subpackage  com_properties
  *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2014 Emtags, All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 
 /**
- * Weblinks Weblink Controller
+ * Properties Property Controller
  *
- * @package     Joomla.Administrator
- * @subpackage  com_weblinks
- * @since       1.5
+ * @package     Realeza
+ * @subpackage  com_properties
+ * @since       1.0
  */
-class WeblinksController extends JControllerLegacy
+class PropertiesController extends JControllerLegacy
 {
 	/**
 	 * Method to display a view.
@@ -26,23 +26,23 @@ class WeblinksController extends JControllerLegacy
 	 *
 	 * @return  JController  This object to support chaining.
 	 *
-	 * @since   1.5
+	 * @since   1.0
 	 */
 	public function display($cachable = false, $urlparams = false)
 	{
-		require_once JPATH_COMPONENT . '/helpers/weblinks.php';
+		require_once JPATH_COMPONENT . '/helpers/properties.php';
 
-		$view   = $this->input->get('view', 'weblinks');
+		$view   = $this->input->get('view', 'properties');
 		$layout = $this->input->get('layout', 'default');
 		$id     = $this->input->getInt('id');
 
 		// Check for edit form.
-		if ($view == 'weblink' && $layout == 'edit' && !$this->checkEditId('com_weblinks.edit.weblink', $id))
+		if ($view == 'property' && $layout == 'edit' && !$this->checkEditId('com_properties.edit.property', $id))
 		{
 			// Somehow the person just went to the form - we don't allow that.
 			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
 			$this->setMessage($this->getError(), 'error');
-			$this->setRedirect(JRoute::_('index.php?option=com_weblinks&view=weblinks', false));
+			$this->setRedirect(JRoute::_('index.php?option=com_properties&view=properties', false));
 
 			return false;
 		}
--- administrator/properties.php
+++ administrator/properties.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * @package     Joomla.Administrator
- * @subpackage  com_weblinks
+ * @package     Realeza
+ * @subpackage  com_properties
  *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2014 Emtags, All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 JHtml::_('behavior.tabstate');
 
-if (!JFactory::getUser()->authorise('core.manage', 'com_weblinks'))
+if (!JFactory::getUser()->authorise('core.manage', 'com_properties'))
 {
 	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
 }
 
-$controller	= JControllerLegacy::getInstance('Weblinks');
+$controller	= JControllerLegacy::getInstance('Properties');
 $controller->execute(JFactory::getApplication()->input->get('task'));
 $controller->redirect();
